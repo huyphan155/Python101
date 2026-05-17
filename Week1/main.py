fa09195a84fe3ab8779e4b1daa916884e20eeee8
HuyPQ <huyphan1505dut@gmail.com>
2026-05-17 19:02:07 +0700

    python101: second commit

--- Week1/main.py
+++ Week1/main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+name = "sum"
+type = "Calico cat"
+is_dog = False
+is_cat = True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def print_sumInfo(name, type, is_dog, is_cat, sound):
+    print(f'Hi,iam {name}')
+    print(f'Iam a {type}')
+    print(f'{is_dog}, Iam a dog')
+    print(f'{is_cat}, Iam a cat')
+    for i in range(sound):
+        print('meoz')
+    print(f'{sound+1} meozzzzzzzzzz, cause im a rebel cat')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+sound = int (input("enter number of meoz : "))
+print_sumInfo(name,type,is_dog,is_cat, sound)
+
