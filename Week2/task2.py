176c6759d66360f80060d67d09fe8ab4da9dff37
HuyPQ <huyphan1505dut@gmail.com>
2026-05-20 21:59:00 +0700

    Week2 task2 COM serial

--- Week2/task2.py
+++ Week2/task2.py
@@ -1,3 +1,16 @@
-import math
+# pip install pyserial
 
-print(math.sqrt(16))
+from serial.tools import list_ports
+
+ports = list_ports.comports()
+
+for port in ports:
+    print(port) # COM1 - Communications Port (COM1)
+    print(port.device) # COM1
+    print(port.description) # Communications Port (COM1)
+    print(port.hwid) # abc\abc\0
+    print(vars(port))
+
+
+
+
