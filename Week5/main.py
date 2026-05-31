bf0cc0e8ef35324f29c4e1baf778b979640e17fb
HuyPQ <huyphan1505dut@gmail.com>
2026-05-31 13:48:27 +0700

    python101: update requirements.txt

---
+++ Week5/main.py
@@ -0,0 +1,10 @@
+import requests
+import serial
+
+def main():
+    print(requests.__version__)
+    print(serial.__version__)
+    print(requests.__file__)
+
+if __name__ == "__main__":
+    main()
