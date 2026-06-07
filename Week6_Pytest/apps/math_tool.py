2c7e054b03b1b759fead33a1804051cba4329b33
Huyphan155_AirM2 <huyphan1505dut@gmail.com>
2026-06-07 16:45:47 +0700

    python101: week6 mock & patch

--- Week6_Pytest/apps/math_tool.py
+++ Week6_Pytest/apps/math_tool.py
@@ -2,3 +2,9 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Invalid input, Divisor cannot be zero")
     return a/b
+
+def get_number():
+    return 10
+
+def calculate():
+    return get_number() * 2
--- Week6_Pytest/tests/test_math.py
+++ Week6_Pytest/tests/test_math.py
@@ -1,6 +1,8 @@
 import pytest
 
 from apps.math_tool import divide
+from apps.math_tool import calculate
+from unittest.mock import patch
 
 
 def test_divide_ok():
@@ -10,3 +12,11 @@ def test_divide_ok():
 def test_divide_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
+
+def test_calculate_mock():
+    with patch(
+            "apps.math_tool.get_number",
+            return_value=100
+    ):
+        result = calculate()
+        assert result == 200
